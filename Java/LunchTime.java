552c21d8e495a34f640bb9e348a104192cbb5d7d
Roshan Ekka <1ROSHANEKKA@GMAIL.COM>
2022-07-11 03:45:29 +0530

    recommit

---
+++ Java/LunchTime.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+import java.util.Scanner;
+
+public class LunchTime
+{
+    public static void main(String args[])
+    {
+        Scanner reading = new Scanner(System.in);
+        int time = reading.nextInt();
+        
+    }
+}
---
+++ Python/PoliceAndThief.py
@@ -0,0 +1,7 @@
+n = int(input())
+
+for i in range(n):
+    police, theif = map(int,input().split())
+    print(abs(police-thief))
+
+#incomplete
