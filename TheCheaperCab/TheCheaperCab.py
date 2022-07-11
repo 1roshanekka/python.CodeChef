6c73dad28fc3565769fda4715d33ff4932b15aaf
Roshan Ekka <1ROSHANEKKA@GMAIL.COM>
2022-07-11 19:12:29 +0530

    added comments to code

--- TheCheaperCab/TheCheaperCab.py
+++ TheCheaperCab/TheCheaperCab.py
@@ -1,21 +1,19 @@
-from sys import api_version
 
-from numpy import append
-
-
+#T is the number of test cases
 T = int(input())
-l = []
+l = [] #list to store the results
+
 for each in range(T):
     X, Y = map(int, input().split())
-    if(X>Y):{
+    if(X>Y):{ #between cab X & Y ,if cab X is costing more than cab Y then second one is cheaper
         l.append("SECOND")
     }
-    elif(Y>X):{
+    elif(Y>X):{#between cab X & Y ,if cab Y is costing more than cab X then first one is cheaper
         l.append("FIRST")
     }
-    elif(X==Y):{
+    elif(X==Y):{#between cab X & Y , both cost the same so any one can be used to ride
         l.append("ANY")
     }
 
 for each in l:
-    print(each)
+    print(each) #printing results of each test cases
